e7806996ece940235411f52b809bddbcf5d8bd94
jonathan kadiata <81642001+kadilab@users.noreply.github.com>
2025-01-19 05:20:21 +0100

    add new config

--- config.php
+++ config.php
@@ -1,5 +1,12 @@
 <?php
-define("DB_DSN", "mysql:host=localhost;dbname=quick");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
+// define("DB_DSN", "mysql:host=localhost;dbname=quick");
+// define("DB_USER", "root");
+// define("DB_PASSWORD", "");
+
+define("DB_DSN", "mysql:host=localhost;dbname=u318332214_quick
+");
+define("DB_USER", "u318332214_master");
+define("DB_PASSWORD", "TZBXz5|K");
+
+
 ?>
