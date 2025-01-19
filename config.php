0e11fda8370b8e660c2e35714df7c639abf0be9f
jonathan kadiata <81642001+kadilab@users.noreply.github.com>
2025-01-19 05:26:00 +0100

    dbname

--- config.php
+++ config.php
@@ -3,8 +3,7 @@
 // define("DB_USER", "root");
 // define("DB_PASSWORD", "");
 
-define("DB_DSN", "mysql:host=localhost;dbname=u318332214_quick
-");
+define("DB_DSN", "mysql:host=localhost;dbname=u318332214_quick");
 define("DB_USER", "u318332214_master");
 define("DB_PASSWORD", "TZBXz5|K");
 
